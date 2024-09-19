e6de8b2b283180bbf7fa37c17ce68fbbe3ac7da5
appoclayptor <shahreza1414@gmail.com>
2024-09-19 22:23:15 +0600

    Added Home page

--- js/login.js
+++ js/login.js
@@ -4,9 +4,21 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // Step-2: Prevent Default behavior(prevent reloading browser)
     event.preventDefault(); // vejal to beginner
-    console.log('login button clicked');
+    // console.log('login button clicked');
 
-    // Step-3: Get the Phone number
+    // Step-3: Get the Phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
-})
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // Step-4: Validate phone and pin
+    // This is temporary way. We'll not do it this way
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are Logged in')
+        // Step-5: Allow User to use the website
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong Phone number or PIN number')
+    }
+});
